150f8fc7b266e7939d7dd902f4ddcb057365f4b7
flooijdt <flooijdt@posteo.de>
2022-04-20 13:53:25 -0300

    add_transactions function added

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -1,13 +1,17 @@
 import datetime
 import hashlib
 import json
+from urllib.parse import urlparse
+from uuid import uuid4
 
-from flask import Flask, jsonify
+import requests
+from flask import Flask, jsonify, request
 
 
 class Blockchain:
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(
             proof=1, previous_hash="0"
         )  # the proof here (PoW) is the nonce.
@@ -18,7 +22,9 @@ class Blockchain:
             "timestamp": str(datetime.datetime.now()),
             "proof": proof,
             "previous_hash": previous_hash,
+            "transactions": self.transactions,
         }
+        self.transactions = []
         self.chain.append(block)
         return block
 
@@ -33,7 +39,7 @@ class Blockchain:
         check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(
-                str(new_proof ** 2 - previous_proof ** 2).encode()
+                str(new_proof**2 - previous_proof**2).encode()
             ).hexdigest()
             # Here the only prerequisite for the operation between prev and new hashes is that the operation cannot be simetrical (X operation Y == Y operation X)
             if hash_operation[:4] == "0000":
@@ -56,13 +62,20 @@ class Blockchain:
             previous_proof = previous_block["proof"]
             proof = block["proof"]
             hash_operation = hashlib.sha256(
-                str(proof ** 2 - previous_proof ** 2).encode()
+                str(proof**2 - previous_proof**2).encode()
             ).hexdigest()
             if hash_operation[:4] != "0000":
                 return False
             previous_block = block
             block_index += 1
         return True
+
+    def add_transactions(self, sender, receiver, amount):
+        self.transactions.append(
+            {"sender": sender, "receiver": receiver, "amount": amount}
+        )
+        previous_block = self.get_previous_block()
+        return previous_block["index"] + 1
 
 
 ### Mining the blockchain
@@ -106,3 +119,5 @@ def is_chain_valid():
 
 # run the app
 app.run(host="0.0.0.0", port=5000)
+
+# descentralizes the blockchain
