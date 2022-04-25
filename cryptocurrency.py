39605c722ce33acc415876cd08e75c617f53d9a8
flooijdt <flooijdt@posteo.de>
2022-04-25 15:41:17 -0300

    create post method add_transaction

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -147,6 +147,20 @@ def is_chain_valid():
     return jsonify(response), 200
 
 
+# adds new transaction to the blockchain
+@app.route("/add_transaction/", methods=["POST"])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ["sender", "receiver", "amount"]
+    if not all(key in json for key in transaction_keys):
+        return "some elements of the transaction are missing", 400
+    index = blockchain.add_transactions(
+        json["sender"], json["receiver"], json["amount"]
+    )
+    response = {"message": f"this transaction will be added to block {index}."}
+    return jsonify(response), 201
+
+
 # run the app
 app.run(host="0.0.0.0", port=5000)
 
